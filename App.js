f54776369070bf24d34b6497d4bfa293139b4016
Linus Unnebäck <linus@folkdatorn.se>
2018-12-13 21:19:46 +0000

    🎉 Improve the app

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import React, {Component} from 'react'
-import {Platform, SafeAreaView, StyleSheet, DeviceEventEmitter, Text, View, Image} from 'react-native'
+import {ActivityIndicator, Button, Platform, SafeAreaView, StyleSheet, DeviceEventEmitter, Text, View, Image} from 'react-native'
 import ReactNativeHeading from 'react-native-heading'
 import geolib from 'geolib'
+import Spacer from 'react-spacer'
 
 import Compass from './components/compass'
 
 const locations = require('./locations.json')
 
-function getClosestPint (location) {
-  return locations[0]
-  // FIXME: implement this
+function getClosestPint (userLocation, blacklist) {
+  let shortestDistance = Infinity
+  let nearestLocation = null
 
-  // for (const location of locations) {
+  for (const location of locations) {
+    if (blacklist.includes(location.id)) continue
 
-  // }
+    const distance = geolib.getDistanceSimple(userLocation, location)
+
+    if (distance < shortestDistance) {
+      shortestDistance = distance
+      nearestLocation = location
+    }
+  }
+
+  return nearestLocation
 }
 
 const styles = StyleSheet.create({
@@ -29,7 +39,18 @@ const styles = StyleSheet.create({
     position: 'absolute'
   },
   distance: {
-    padding: 20,
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  venueName: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  pintName: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  pintPrice: {
     fontSize: 24,
     fontWeight: 'bold',
   },
@@ -38,7 +59,14 @@ const styles = StyleSheet.create({
 export default class App extends Component {
   constructor (props) {
     super(props)
-    this.state = { heading: 0, pintHeading: null, pintDistance: null }
+
+    this.state = {
+      heading: 0,
+      closestLocation: null,
+      pintHeading: null,
+      pintDistance: null,
+      visitedVenuesIds: [],
+    }
   }
 
   componentDidMount () {
@@ -49,21 +77,62 @@ export default class App extends Component {
     })
 
     navigator.geolocation.watchPosition((position) => {
-      const closestPint = getClosestPint(position.coords)
-      const heading = geolib.getRhumbLineBearing(position.coords, closestPint)
-      const distance = geolib.getDistanceSimple(position.coords, closestPint)
+      const closestLocation = getClosestPint(position.coords, this.state.visitedVenuesIds)
+      const heading = geolib.getRhumbLineBearing(position.coords, closestLocation)
+      const distance = geolib.getDistanceSimple(position.coords, closestLocation)
 
-      this.setState({ pintHeading: heading, pintDistance: distance })
+      this.setState({ closestLocation, pintHeading: heading, pintDistance: distance })
+    })
+  }
+
+  userDidDrinkPint = () => {
+    console.log('The user has had a pint!')
+
+    this.setState({
+      visitedVenuesIds: [...this.state.visitedVenuesIds, this.state.closestLocation.id]
     })
   }
 
   render () {
-    const { heading, pintHeading, pintDistance } = this.state
+    const { heading, closestLocation, pintHeading, pintDistance } = this.state
+
+    if (!closestLocation) {
+      return (
+        <SafeAreaView style={styles.container}>
+          <Spacer height={20} />
+          <ActivityIndicator size='large' />
+        </SafeAreaView>
+      )
+    }
 
     return (
       <SafeAreaView style={styles.container}>
+        <Spacer height={20} />
         <Compass heading={heading} targetHeading={pintHeading} />
-        <Text style={styles.distance}>{pintDistance} meters</Text>
+        <Spacer height={20} />
+
+        {pintDistance > 25 ? <>
+          <Text>Walk</Text>
+          <Text style={styles.distance}>{Math.round(pintDistance / 1.09361)} yards</Text>
+          <Spacer height={20} />
+          <Text>then drink a pint of</Text>
+          <Text style={styles.pintName}>{closestLocation.cheapestBeerName}</Text>
+          <Spacer height={20} />
+          <Text>and pay</Text>
+          <Text style={styles.pintPrice}>£{(closestLocation.cheapestBeerPrice / 100).toFixed(2)}</Text>
+          <Spacer height={20} />
+        </> : <>
+          <Text>Go in to</Text>
+          <Text style={styles.venueName}>{closestLocation.venueName}</Text>
+          <Spacer height={20} />
+          <Text>then drink a pint of</Text>
+          <Text style={styles.pintName}>{closestLocation.cheapestBeerName}</Text>
+          <Spacer height={20} />
+          <Text>and pay</Text>
+          <Text style={styles.pintPrice}>£{(closestLocation.cheapestBeerPrice / 100).toFixed(2)}</Text>
+          <Spacer height={20} />
+          <Button title="I've had the pint, next!" onPress={this.userDidDrinkPint} />
+        </>}
       </SafeAreaView>
     )
   }
