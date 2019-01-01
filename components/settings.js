17ebf5b0ea4c2bba7cf033088ba164ac0b7eb8c6
Richard Skogeby <richard.skogeby@outlook.com>
2019-01-01 15:51:19 +0000

    Added button for adding location on settings menu

--- components/settings.js
+++ components/settings.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
-import { Text, SafeAreaView } from 'react-native'
+import { Text, SafeAreaView, Button } from 'react-native'
+import styles from '../styles'
 
 export default class SettingsScreen extends Component {
   render () {
     return (
-      <SafeAreaView>
+      <SafeAreaView style={styles.container}>
         <Text>Hello, World!</Text>
+        <Text>Hi</Text>
+        <Button title='Add location' onPress={this.userAddCurrentLocation} />
       </SafeAreaView>
     )
   }
